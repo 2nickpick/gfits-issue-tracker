4cfcb09fbbc1b45c5b3a9efca1150be3a5c9b4c0
Nicholas Pickering <npickering@elykinnovation.com>
2015-10-10 14:33:07 -0400

    two home page buttons

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/group4/head.php' ); ?>
 
 <div class="inner cover">
-	<h1 class="cover-heading">So easy, even kids can use it!</h1>
+	<h1 class="cover-heading">Welcome to GFITS!</h1>
 
-	<p class="lead">GFITS is the perfect solution to your issue tracking needs.</p>
+	<p class="lead">Group Four Issue Tracking System</p>
 
 	<p class="lead">
-		<a href="/group4/sign-up.php" class="btn btn-lg btn-primary">Try it now!</a>
+		<a href="/group4/sign-up.php" class="btn btn-lg btn-primary">Register</a>
+		<a href="/group4/log-in.php" class="btn btn-lg btn-success">Log In</a>
 	</p>
 </div>
 
